510018023bbad494542ed177e6112263b0161ac0
Abdul Rahman <kadkab.abdul@gmail.com>
2019-03-09 23:32:27 +0530

    Add functionality to get all users due amount

---
+++ cmd/report/total_dues.go
@@ -0,0 +1,26 @@
+package report
+
+import (
+	"github.com/abdulrahmank/pay_later/cmd"
+	"github.com/spf13/cobra"
+	"log"
+)
+
+var cmdTotalDues = &cobra.Command{
+	Use:   "total-dues",
+	Short: "To get total dues of all the users",
+	Run:   GetTotalDues,
+}
+
+func init() {
+	CmdReport.AddCommand(cmdTotalDues)
+}
+
+func GetTotalDues(_ *cobra.Command, _ []string) {
+	totalDues := float32(0)
+	for _, val := range cmd.UserDao.GetAllUsers() {
+		log.Printf("%s: %f", val.Name, val.Dues)
+		totalDues = totalDues + val.Dues
+	}
+	log.Printf("Total due amount: %f", totalDues)
+}
